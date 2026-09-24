2066756dba0c097e10a7c1bb4a92243321861f28
fsologureng <fsologur@uchile.cl>
2026-09-24 11:53:57 -0300

    build(koa): remove virtual-labx-theme

--- cms-assets.py
+++ cms-assets.py
@@ -10,8 +10,7 @@ COMPREHENSIVE_THEME_LOCALE_PATHS = [
 	"/openedx/themes/alpha-project-theme/conf/locale",
 	"/openedx/themes/cmm-openedx/conf/locale",
 	"/openedx/themes/eduonline-ing/conf/locale",
-	"/openedx/themes/eol-uchile-2020/conf/locale",
-	"/openedx/themes/virtual-labx-theme/conf/locale"
+	"/openedx/themes/eol-uchile-2020/conf/locale"
 ]
 STATIC_ROOT_BASE = '/openedx/staticfiles'
 STATIC_ROOT = path(STATIC_ROOT_BASE) / 'studio'
